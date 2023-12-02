27ae0b6f09a6a4a754970ba42b2a47e158ef887e
Bryan Ochoa <87205378+akajar@users.noreply.github.com>
2023-12-02 16:37:37 -0500

    Update challenge01.js

--- challenge01.js
+++ challenge01.js
@@ -1,11 +1,9 @@
-function findFirstRepeated(gifts) {
+function findFirstRepeated(gifts) { //v1 - 220 pts
   // Code here
   const uniqueGifts = []
-
   for(let gift of gifts){
     if (uniqueGifts.includes(gift)) return gift;
     else uniqueGifts.push(gift);
   }
-
   return -1;
 }
