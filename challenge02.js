ad097a1600f08ff2877c909a39739add9d077aaf
Bryan Ochoa <87205378+akajar@users.noreply.github.com>
2023-12-07 12:11:38 -0500

    Update challenge02.js

--- challenge02.js
+++ challenge02.js
@@ -1,9 +1,4 @@
 function manufacture(gifts, materials) { // v1 - 270 pts
   // Code here
-  return gifts.filter(gift => {
-    for(let t of gift.split('')){
-      if(!materials.includes(t)) return false;
-    }
-    return true;
-  });
+  return gifts.filter(gift => gift.split('').every(i => materials.includes(i)));
 }
