1197a6e8506eea9240d81851a361c55440bb4238
Bryan Ochoa <87205378+akajar@users.noreply.github.com>
2023-12-07 12:24:48 -0500

    version 2

--- challenge03.js
+++ challenge03.js
@@ -1,8 +1,17 @@
-function findNaughtyStep(original, modified) { //260 pts
+function findNaughtyStep(original, modified) { //350 pts (???)
   // Code here
   if(original == modified) return '';
   const larger = original.length > modified.length? original: modified;
-  for(const index in larger){
+  for(let index=0; index < larger.length; index++){
     if(original[index] != modified[index]) return larger[index];
   }
 }
+
+// function findNaughtyStep(original, modified) { //260 pts
+//   // Code here
+//   if(original == modified) return '';
+//   const larger = original.length > modified.length? original: modified;
+//   for(const index in larger){
+//     if(original[index] != modified[index]) return larger[index];
+//   }
+// }
