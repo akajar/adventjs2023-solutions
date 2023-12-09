be9bc7fb5721877eb40552e734a2f3fb7e25883d
Bryan Ochoa <87205378+akajar@users.noreply.github.com>
2023-12-09 01:55:23 -0500

    version 2

--- challenge08.js
+++ challenge08.js
@@ -1,20 +1,36 @@
-function organizeGifts(gifts) {//v1 210 pts
+function organizeGifts(gifts) {//v2 250 pts
   // Code here
   const orgGifts = gifts
     .match(/\d+\w/g)
-    .map(m =>{
-    return {
-      name: m.slice(-1),
-      cant: parseInt(m.slice(0,-1))
-    };
-  })
+    .map(m => [m.slice(-1), m.slice(0,-1)*1]);
 
   let result = '';
 
   for(const gift of orgGifts){
-    result += `[${gift.name}]`.repeat(gift.cant/50)
-    result += `{${gift.name}}`.repeat((gift.cant%50)/10)
-    result += (gift.cant%50)%10 != 0?'('+gift.name.repeat((gift.cant%50)%10)+')':'';
+    const [name,cant] = [gift[0],gift[1]]
+    if(cant >= 50) result += `[${name}]`.repeat(cant/50)
+    if(cant >= 10) result += `{${name}}`.repeat((cant%50)/10)
+    if(cant%50%10 > 0) result += '('+name.repeat(cant%50%10)+')'
   }
   return result;
 }
+// function organizeGifts(gifts) {//v1 210 pts
+//   // Code here
+//   const orgGifts = gifts
+//     .match(/\d+\w/g)
+//     .map(m =>{
+//     return {
+//       name: m.slice(-1),
+//       cant: parseInt(m.slice(0,-1))
+//     };
+//   })
+
+//   let result = '';
+
+//   for(const gift of orgGifts){
+//     result += `[${gift.name}]`.repeat(gift.cant/50)
+//     result += `{${gift.name}}`.repeat((gift.cant%50)/10)
+//     result += (gift.cant%50)%10 != 0?'('+gift.name.repeat((gift.cant%50)%10)+')':'';
+//   }
+//   return result;
+// }
